bc18807ccda67de71f7d2fa400fe030aeb605b65
just-tenacious <tshraddha1505@gmail.com>
2025-10-01 16:04:52 +0530

    Commit after setup

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,5 +8,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(greet());
         System.out.ptintln("Setup Complete");
+        System.out.ptintln("Test");
+        
     }
 }
